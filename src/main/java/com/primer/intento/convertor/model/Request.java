f2dc372e029badf033d513d0ae8d9a64431f9045
sebastianremar <rsrdlf93@gmail.com>
2023-02-23 10:37:11 -0500

    Added builder annotation for request

--- src/main/java/com/primer/intento/convertor/model/Request.java
+++ src/main/java/com/primer/intento/convertor/model/Request.java
@@ -1,8 +1,10 @@
 package com.primer.intento.convertor.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Request {
     private String value;
     private String currentUnit;
