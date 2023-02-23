5ec31d298b2788a3115f0d5871a84e8b84d7ad97
sebastianremar <rsrdlf93@gmail.com>
2023-02-22 19:50:59 -0500

    Add event listener for right select

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,38 +1,66 @@
-document.getElementsByName("temp")[0].addEventListener('keyup', () => {
+const leftInput = document.getElementById('idLeftInput');
+const rightInput = document.getElementById('idRightInput');
 
+const handleInputChange = () => {
     // Getting values with QuerySelector
-    const leftTemp = document.getElementById('leftValue').value;
-    const rightTemp = Number(document.getElementById('rightValue').value);
+    const leftTemp = document.getElementById('idLeftInput').value;
+    const rightTemp = Number(document.getElementById('idRightInput').value);
 
     // Getting temperature units
-    const leftUnit = document.getElementById("tempLeft");
-    const leftText = leftUnit.options[leftUnit.selectedIndex].text;
+    const leftElementId = document.getElementById("idTempLeft");
+    const leftUnitText = leftElementId.options[leftElementId.selectedIndex].text;
 
     // Getting temperature units
-    const rightUnit = document.getElementById("tempRight");
-    const rightText = rightUnit.options[rightUnit.selectedIndex].text;
+    const rightElementId = document.getElementById("idTempRight");
+    const rightUnitText = rightElementId.options[rightElementId.selectedIndex].text;
 
     // CASO #1 INPUT ESTA VACIO
-    if (!leftTemp) {
-    //  Asumamos que estamos en un estado en el que el usuario tiene en blanco el input
-        document.getElementById("rightValue").value = "";
+    if (!leftTemp && event.target === leftInput) {
+        //  Asumamos que estamos en un estado en el que el usuario tiene en blanco el input
+        document.getElementById("idRightInput").value = "";
+        return;
+    }
+    if (!rightTemp && event.target === rightInput) {
+        document.getElementById("idLeftInput").value = "";
         return;
     }
 
     const leftTempNumber = Number(leftTemp);
+    const rightTempNumber = Number(rightTemp);
 
     // CASO #2 El input es una letra
     if (isNaN(leftTempNumber)) {
         console.log("Input es una letra")
-        document.getElementById("rightValue").value = "";
+        document.getElementById("idRightInput").value = "";
         return;
     }
 
-   const data = {
-        value : leftTemp,
-        currentUnit : leftText,
-        unitToChange : rightText
-   }
+    if (isNaN(rightTempNumber)) {
+        console.log("Input es una letra")
+        document.getElementById("idLeftInput").value = "";
+        return;
+    }
+
+    let tempToChange;
+    let currentUnit;
+    let unitToChange;
+    let isLeft;
+    if (event.currentTarget === leftInput) {
+        tempToChange = leftTempNumber;
+        currentUnit = leftUnitText;
+        unitToChange = rightUnitText;
+        isLeft = true;
+    } else {
+        tempToChange = rightTempNumber;
+        currentUnit = rightUnitText;
+        unitToChange = leftUnitText;
+    }
+
+    const data = {
+        value : tempToChange,
+        currentUnit : currentUnit,
+        unitToChange : unitToChange
+    }
 
     $.ajax('tmp', {
         type: "POST",
@@ -40,12 +68,17 @@ document.getElementsByName("temp")[0].addEventListener('keyup', () => {
         data: JSON.stringify(data),
         success: (res) => {
             const {temp, tempUnit} = res;
-            document.getElementById("rightValue").value = temp;
+            if (isLeft) {
+                document.getElementById("idRightInput").value = temp;
+            } else {
+                document.getElementById("idLeftInput").value = temp;
+            }
 
         },
         error: err => {
             console.log(err);
         }
-   });
-})
-
+    });
+}
+leftInput.addEventListener('keyup', handleInputChange);
+rightInput.addEventListener('keyup', handleInputChange);
